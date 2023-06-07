844f459ea0d7723f4da9fa5c684f4da4e24bbc2a
chelyvanis1987 <ara_vic37@hotmail.com>
2023-06-07 10:01:38 -0700

    Modificaciones clase 7 refactor del mapa del juego

--- game.js
+++ game.js
@@ -31,13 +31,23 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    const map = maps[1];
+    const map = maps[0];
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows, mapRowCols});
 
-    for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col++)
-        game.fillText(emojis[mapRowCols[row - 1][col - 1]],  elementsSize * col, elementsSize * row);
-    }
+    mapRowCols.forEach(row, rowI => {
+        row.forEach(col, colI => {
+            const emojis = emojis[col];
+            constposX = elementsSize * (colI + 1);
+            const posY = elementsSize * (rowI + 1);
+            game.fillText(emojis, posX, posY);
+        });
+    });
+
+
+   // for (let row = 1; row <= 10; row++) {
+      //  for (let col = 1; col <= 10; col++)
+      //  game.fillText(emojis[mapRowCols[row - 1][col - 1]],  elementsSize * col, elementsSize * row);
+   // }
 }
